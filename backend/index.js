fa1836ee688c76037a267359c3dab0b4bef8f99e
khushboo8252 <khushbooranjan8252@gmail.com>
2024-08-25 15:51:43 +0530

    ready for deploy

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
-// const express = require('express')// method-1
-import express from "express"; // method-2
+
+import express from 'express';
 import dotenv from "dotenv"; 
 import connectDB from "./config/database.js";
 import userRoute from "./routes/userRoute.js";
@@ -7,10 +7,15 @@ import messageRoute from "./routes/messageRoute.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app,server } from "./socket/socket.js";
+import path from "path";
+
 dotenv.config({});
+
 
  
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 // middleware
 app.use(express.urlencoded({extended:true}));
@@ -27,8 +32,10 @@ app.use(cors(corsOption));
 app.use("/api/v1/user",userRoute); 
 app.use("/api/v1/message",messageRoute);
  
-
-
+app.use(express.static(path.join(__dirname,"/frontend/build")));
+app.get("*",(req,res) =>{
+    res.sendFile(path.resolve(__dirname,"frontend","build","index.html"));
+})
 
 server.listen(PORT, ()=>{
     connectDB();
